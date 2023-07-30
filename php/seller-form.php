c6b581b52127740210747a38899dc6fefc8c623a
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-07-30 10:14:40 +0330

    Update the sort function to preserve the previous array keys

--- php/seller-form.php
+++ php/seller-form.php
@@ -13,40 +13,41 @@ if (mysqli_num_rows($result) > 0) {
   }
 }
 
+print_r(json_encode($data));
+
 $data = persianSort($data);
 
-function persianSort($array)
-{
+function persianSort($array) {
   $persianAlphabet = array(
-    'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و', 'ه', 'ی'
+      'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و', 'ه', 'ی'
   );
 
-  usort($array, function ($a, $b) use ($persianAlphabet) {
-    $aLength = mb_strlen($a);
-    $bLength = mb_strlen($b);
-    $maxLength = max($aLength, $bLength);
+  uasort($array, function($a, $b) use ($persianAlphabet) {
+      $aLength = mb_strlen($a);
+      $bLength = mb_strlen($b);
+      $maxLength = max($aLength, $bLength);
 
-    for ($i = 0; $i < $maxLength; $i++) {
-      $aChar = mb_substr($a, $i, 1);
-      $bChar = mb_substr($b, $i, 1);
+      for ($i = 0; $i < $maxLength; $i++) {
+          $aChar = mb_substr($a, $i, 1);
+          $bChar = mb_substr($b, $i, 1);
 
-      $aIndex = array_search($aChar, $persianAlphabet);
-      $bIndex = array_search($bChar, $persianAlphabet);
+          $aIndex = array_search($aChar, $persianAlphabet);
+          $bIndex = array_search($bChar, $persianAlphabet);
 
-      if ($aIndex !== false && $bIndex !== false) {
-        if ($aIndex < $bIndex) {
-          return -1;
-        } elseif ($aIndex > $bIndex) {
-          return 1;
-        }
-      } elseif ($aIndex !== false) {
-        return -1;
-      } elseif ($bIndex !== false) {
-        return 1;
+          if ($aIndex !== false && $bIndex !== false) {
+              if ($aIndex < $bIndex) {
+                  return -1;
+              } elseif ($aIndex > $bIndex) {
+                  return 1;
+              }
+          } elseif ($aIndex !== false) {
+              return -1;
+          } elseif ($bIndex !== false) {
+              return 1;
+          }
       }
-    }
 
-    return 0;
+      return 0;
   });
 
   return $array;
