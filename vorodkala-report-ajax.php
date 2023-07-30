1b99f6b5cad47fde1f8dd11efde0cda38244e015
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-07-30 13:39:53 +0330

    Update the filter operation by extracting it

--- vorodkala-report-ajax.php
+++ vorodkala-report-ajax.php
@@ -1,6 +1,8 @@
 <?php
 require_once("./php/db.php");
 
+print_r(json_encode($_POST));
+
 $partNumber = $_POST['partNumber'] === 'null' ? null : $_POST['partNumber']; // Assuming you're retrieving the value from a form
 $seller_id = $_POST['seller'] === 'null' ? null : $_POST['seller'];
 $brand_id = $_POST['brand'] === 'null' ? null : $_POST['brand'];
