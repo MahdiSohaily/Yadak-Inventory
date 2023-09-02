69d444adb9b11ec02ab2856cbc0323865435e011
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-09-02 10:50:40 +0330

    Add assets to the specific folder

--- login.php
+++ login.php
@@ -113,8 +113,8 @@ if (!empty($login_err)) {
     <style>
         @import url('https://v1.fontapi.ir/css/Vazir');
     </style>
-    <link href="./public/css/assets/bootstrap.min.css?v=<?= rand() ?>" rel="stylesheet">
-    <script src="./public/js/assets/bootstrap.bundle.min.js?v=<?= rand() ?>"></script>
+    <link href="./public/css/assets/bootstrap.min.css" rel="stylesheet">
+    <script src="./public/js/assets/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./public/css/login.css">
 </head>
 
