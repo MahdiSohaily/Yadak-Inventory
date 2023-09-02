d840db2d1e970123d0d21dc596ac639aac525898
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-09-02 13:16:42 +0330

    FIX the join problem in transfer report

--- php/transfer_report_getter.php
+++ php/transfer_report_getter.php
@@ -10,7 +10,7 @@ if (isset($interval)) {
 
 try {
     $statement = PDO_CONNECTION->prepare("SELECT transfer_record.*, qtybank.qty AS previous_amount,
-    nisha.partnumber, brand.name As brand_name, seller.name AS seller_name, deliverer.name AS deliverer_name,
+    nisha.partnumber, brand.name As brand_name, seller.name AS seller_name, getter.name AS getter_name,
     users.name AS user_name, qtybank.stock_id, exitrecord.des
     FROM transfer_record
     INNER JOIN qtybank ON qtybank.id =  transfer_record.affected_record
@@ -18,7 +18,7 @@ try {
     INNER JOIN exitrecord ON exitrecord.id  = transfer_record.exit_id
     LEFT JOIN brand ON brand.id = qtybank.brand
     LEFT JOIN seller ON seller.id = qtybank.seller
-    LEFT JOIN deliverer ON deliverer.id = exitrecord.getter
+    LEFT JOIN getter ON getter.id = exitrecord.getter
     INNER JOIN users ON users.id = transfer_record.user_id
     ORDER BY transfer_record.transfer_date DESC");
     $statement->execute();
@@ -38,10 +38,10 @@ foreach ($results as $index => $result) : ?>
         <td class="cell-des "><?= $result["des"] ?></td>
         <td class="cell-qty "><?= $result["previous_amount"] ?></td>
         <td class="cell-pos1 "><?= $result["quantity"] ?></td>
-        <td class="cell-pos1 "><?= getStockName($result["stock"]) ?></td>
         <td class="cell-pos1 "><?= getStockName($result["stock_id"]) ?></td>
+        <td class="cell-pos1 "><?= getStockName($result["stock"]) ?></td>
         <td class="cell-pos2 "><?= $result["seller_name"] ?></td>
-        <td class="cell-pos2 "><?= $result["deliverer_name"] ?></td>
+        <td class="cell-pos2 "><?= $result["getter_name"] ?></td>
         <td class="cell-user "><?= $result["user_name"] ?></td>
     </tr>
 <?php endforeach;
