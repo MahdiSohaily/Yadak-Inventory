ec3a64d337b8eb5ef7104937aed0fc07db1fb879
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2023-09-03 17:56:02 +0330

    Update the script to display existing goods to upgrade the performance

--- js/khorojkala.js
+++ js/khorojkala.js
@@ -1,10 +1,10 @@
- $(document).ready(function () {
+//  $(document).ready(function () {
 
-     $(".edit-rec2").click(function () {
-         $(".modal").css("display", "block");
-         $(this).attr("id");
-         $("iframe").attr("src", "php/khorojkala-report-edit.php?q=" + $(this).attr("id"));
+//      $(".edit-rec2").click(function () {
+//          $(".modal").css("display", "block");
+//          $(this).attr("id");
+//          $("iframe").attr("src", "php/khorojkala-report-edit.php?q=" + $(this).attr("id"));
 
-     });
+//      });
 
- });
+//  });
--- js/vorodkala.js
+++ js/vorodkala.js
@@ -1,12 +1,12 @@
  
 
-  $(document).ready(function () {
+//   $(document).ready(function () {
  
-      $(".edit-rec").click(function () {
-          $(".modal").css("display","block");
-           $(this).attr("id");
-          $("iframe").attr("src","php/vorodkala-report-edit.php?q="+$(this).attr("id"));
+//       $(".edit-rec").click(function () {
+//           $(".modal").css("display","block");
+//            $(this).attr("id");
+//           $("iframe").attr("src","php/vorodkala-report-edit.php?q="+$(this).attr("id"));
 
-      });
+//       });
 
-  });
+//   });
