bb055e8a160fce406bb31ada869900358ca9bb46
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-09-30 13:29:32 +0330

    Update the login page favicon

--- login.php
+++ login.php
@@ -82,7 +82,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate credentials
     if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password, isLogin FROM users WHERE username = ?";
 
         if ($stmt = mysqli_prepare($con, $sql)) {
             // Bind variables to the prepared statement as parameters
@@ -99,30 +99,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // Check if username exists, if yes then verify password
                 if (mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $isLogin);
                     if (mysqli_stmt_fetch($stmt)) {
-                        if (password_verify($password, $hashed_password)) {
-                            // Password is correct, so start a new session
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
+                        if (!$isLogin) {
+                            if (password_verify($password, $hashed_password)) {
+                                // Password is correct, so start a new session
+                                // Store data in session variables
+                                $_SESSION["loggedin"] = true;
+                                $_SESSION["id"] = $id;
+                                $_SESSION["username"] = $username;
 
-                            // ... (other processing)
-                            date_default_timezone_set('Asia/Tehran');
-                            // Call a function to send the AJAX request
-                            sendAjaxRequest($id, $username);
+                                $sql = "Update users SET isLogin = '1' WHERE id = '$id'";
 
-                            // Redirect user to welcome page
-                            // header("location: index.php?msg=$username");
-                            $myfile = fopen("login.txt", "a") or die("Unable to open file!");
-                            $txt = $username . ' ' . date("Y-m-d h:i:sa") . " Logged in \n";
-                            fwrite($myfile, $txt);
-                            fclose($myfile);
+                                $con->query($sql);
+
+                                // ... (other processing)
+                                date_default_timezone_set('Asia/Tehran');
+                                // Call a function to send the AJAX request
+                                sendAjaxRequest($id, $username);
+
+                                // Redirect user to welcome page
+                                // header("location: index.php?msg=$username");
+                                $myfile = fopen("login.txt", "a") or die("Unable to open file!");
+                                $txt = $username . ' ' . date("Y-m-d h:i:sa") . " Logged in \n";
+                                fwrite($myfile, $txt);
+                                fclose($myfile);
+                            } else {
+                                // Password is not valid, display a generic error message
+                                $login_err = "رمز عبور یا اسم کاربری اشتباه است.";
+                            }
                         } else {
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
+                            $login_err = "حساب کاربری مدنظر هم اکنون  در جای دیگری باز است.";
                         }
+
+
+
 
                         // Function to send the AJAX request
 
