c2cd1eca67c6ee1a90c03b597ba3dc35a67c41c7
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-09-30 13:33:01 +0330

    Update the login page favicon

--- logout.php
+++ logout.php
@@ -1,14 +1,17 @@
 <?php
 // Initialize the session
 session_start();
- 
+require_once './php/db.php';
+$id = $_SESSION['id'];
+$sql = "Update users SET isLogin = '0' WHERE id = '$id'";
+
+$con->query($sql);
 // Unset all of the session variables
 $_SESSION = array();
- 
+
 // Destroy the session.
 session_destroy();
- 
+
 // Redirect to login page
 header("location:/YadakShop-APP/index.php");
 exit;
-?>
