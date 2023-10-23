25222ba9ca6863203e51c9d86cd4b5bcc9608b0b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-10-23 11:08:00 +0330

    Update the id of new part number submit button

--- newcode-index.php
+++ newcode-index.php
@@ -21,7 +21,7 @@
 
 
                 <label for="newcode">کد فنی</label>
-                <input onkeydown="upperCaseF(this)"  type="text" name="newcode" id="newcode-in">
+                <input onkeydown="upperCaseF(this)" type="text" name="newcode" id="newcode-in">
 
             </div>
             <div class="right-form">
@@ -33,50 +33,27 @@
                 <label class="small-label" for="doll-gen">دلار جنیون</label>
                 <input class="small-input" type="number" name="doll-gen" id="doll-gen">
 
-               
+
 
                 <label class="small-label" for="doll-psq">دلار پارت</label>
                 <input class="small-input" type="number" name="doll-psq" id="doll-psq">
 
-<p style="Clear:both"></p>
-                 <label class="small-label" for="doll-mob">دلار موبیز</label>
+                <p style="Clear:both"></p>
+                <label class="small-label" for="doll-mob">دلار موبیز</label>
                 <input class="small-input" type="number" name="doll-mob" id="doll-mob">
-                
-                
+
+
                 <label class="small-label" for="doll-pm">دلار پارس</label>
                 <input class="small-input" type="number" name="doll-pm" id="doll-pm">
 
-
-
-
                 <div class="bottom-bar">
-                    <input type="submit" value="افزودن کد فنی" id="sabt">
+                    <input type="submit" value="افزودن کد فنی" id="">
                     <div class="error">
                     </div>
                 </div>
-
-
-
             </div>
         </form>
-
-
-
-
-
-
-
-
-
-
     </div>
-
-
-
-
-
-
-
 </div>
 
 
