5a72db11a915380624ae208c1cb6e7f6df316dd7
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-30 11:46:43 +0330

    Add the single item report page

--- singleItemReport.php
+++ singleItemReport.php
@@ -6,6 +6,41 @@ if (isset($_GET['interval'])) {
     $interval = $_GET['interval'];
 }
 ?>
-<section class="flex">
-<input type="text" name="code" id="code" onkeyup="convertToEnglish(this); searchCode(this.value)">
+<style>
+    section {
+        padding-inline: 15px;
+    }
+
+    .flex {
+        display: flex;
+    }
+
+    .justify-center {
+        justify-content: center;
+    }
+
+    .form-controller {
+        padding: 10px;
+        width: 300px;
+        font-size: 12px;
+        border-radius: 5px;
+        border: 1px solid lightgray;
+    }
+
+    .form-controller::placeholder {
+        font-size: 12px;
+    }
+</style>
+<section class="flex justify-center">
+    <input class="form-controller" type="text" name="code" id="code" onkeyup="convertToEnglish(this); searchCode(this.value)" placeholder="کد مد نظر خود را بصورت کامل وارد کنید">
+</section>
+
+<section id="price">
+    <h2>قیمت قطعه</h2>
+</section>
+<section id="import">
+    <h2>گزارش ورود</h2>
+</section>
+<section id="export">
+    <h2>گزارش خروج</h2>
 </section>
